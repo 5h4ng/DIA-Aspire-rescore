559c8ef4c7c89002527b23206ddfef0085496d28
Zixiang Shang <steven5h4ng@gmail.com>
2025-12-23 23:23:28 -0500

    Add chromatogram plotting functions for MS1 and MS2 XIC visualization

--- dia_aspire_rescore/plot/__init__.py
+++ dia_aspire_rescore/plot/__init__.py
@@ -1,3 +1,9 @@
+from dia_aspire_rescore.plot.chromatogram import plot_xic_ms1, plot_xic_ms2
 from dia_aspire_rescore.plot.stats_plot import plot_qvalues, plot_target_decoy_dist
 
-__all__ = ["plot_qvalues", "plot_target_decoy_dist"]
+__all__ = [
+    "plot_qvalues",
+    "plot_target_decoy_dist",
+    "plot_xic_ms1",
+    "plot_xic_ms2",
+]
