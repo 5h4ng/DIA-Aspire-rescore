0a94288c48c43ffdaae52ac492ce4626f294f84a
Zixiang Shang <steven5h4ng@gmail.com>
2026-01-12 13:18:02 -0500

    Prepare to deprecate psm_utils module

--- dia_aspire_rescore/io/__init__.py
+++ dia_aspire_rescore/io/__init__.py
@@ -1,5 +1,5 @@
+from ._psm_utils.diann2 import DIANN2ParquetReader
 from .diann2_reader import read_diann2
-from .psm_utils.diann2 import DIANN2ParquetReader
 from .utils import find_ms_files
 
 __all__ = ["DIANN2ParquetReader", "find_ms_files", "read_diann2"]
