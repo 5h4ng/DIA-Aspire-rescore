b31651c591dcf2070fec85c5bb8235ebe6959231
Zixiang Shang <steven5h4ng@gmail.com>
2026-01-13 19:06:24 -0500

    Remove psm_utils related module

--- dia_aspire_rescore/io/__init__.py
+++ dia_aspire_rescore/io/__init__.py
@@ -1,10 +1,8 @@
-from ._psm_utils.diann2 import DIANN2ParquetReader
 from .diann2_reader import read_diann2
 from .speclib_reader import SpecLibReader, read_speclib
 from .utils import find_ms_files
 
 __all__ = [
-    "DIANN2ParquetReader",
     "SpecLibReader",
     "find_ms_files",
     "read_diann2",
